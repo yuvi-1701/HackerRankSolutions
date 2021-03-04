18ee70296b34fc9ca6d6076b04943821642fe78e
Yuvraj Darekar <67352189+yuvi-1701@users.noreply.github.com>
2021-03-04 09:38:47 +0530

    Hackerrank solution for deleting node in java

---
+++ DeleteNode.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the deleteNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) 
+    {
+        int cnt = 0;
+        if(head == null)
+        {
+            return head;
+        }
+        SinglyLinkedListNode ptr = head;
+        if(position == 0)
+        {
+            head = head.next;
+            return head;
+        }
+        else
+        {
+            for(int i =0; i< position-1; i++)
+            {
+                ptr = ptr.next;
+            }
+            ptr.next = ptr.next.next;
+            return head;
+        }
+        
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
